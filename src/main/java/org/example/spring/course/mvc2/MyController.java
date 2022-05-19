3c1a2b8163701921d3266955ab08212a1f7bce7c
Alexey <alexey22-86@list.ru>
2022-05-19 16:31:56 +0300

    spring mvc forms

---
+++ src/main/java/org/example/spring/course/mvc2/MyController.java
@@ -0,0 +1,40 @@
+package org.example.spring.course.mvc2;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class MyController {
+
+    @RequestMapping("/")
+    public String showInfo(){
+        return "info-test";
+    }
+
+    @RequestMapping("/askDetails")
+    public String askEmployeeDetails(Model model){
+
+        model.addAttribute("employee", new Employee());
+
+        return "ask-emp-details-view";
+    }
+
+    @RequestMapping("/showDetails")
+    public String showEmpDetails(@ModelAttribute("employee") Employee emp) {
+
+        String name = emp.getName();
+        emp.setName("Mr. " + name);
+
+        String surname = emp.getSurname();
+        emp.setSurname(surname + "!");
+
+        int salary = emp.getSalary();
+        emp.setSalary(salary*10);
+
+        return "show-emp-details-view";
+    }
+}
+
---
+++ src/main/webapp/WEB-INF/view/show-emp-details-view.jsp
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+
+
+<body>
+
+<h2>Dear Employee, you are WELCOME!!!</h2>
+
+<br><br>
+
+<%--Your name: ${param.employeeName}--%>
+
+Your name: ${employee.name}
+<br>
+Your surname: ${employee.surname}
+<br>
+Your salary: ${employee.salary}
+
+</body>
+
+</html>
